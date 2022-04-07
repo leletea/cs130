bfb1c4b4c18981d97cfd520f0d0986e7a57e3abe
leletea <lexiehu2023@u.northwestern.edu>
2022-04-06 19:22:28 -0500

    My First Commit

---
+++ tutorials/tutorial01/main.py
@@ -0,0 +1,9 @@
+# Exercise 1:
+n=int(input('Enter a number: '))
+print('The sum of n+nn+nnn is ',n+n**2+n**3)
+
+# Exercise 2:
+print('''   __    __
+o-''))_____\\
+"--__/ * * * )
+c_c__/-c____/''')
